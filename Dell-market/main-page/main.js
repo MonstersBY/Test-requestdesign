6294046178fd6f935a4e013ac4ba3a19d281378d
Alex Romanchenko <monsters.alex0@gmail.com>
2023-05-01 14:08:37 +0300

    feat: input validation

--- Dell-market/main-page/main.js
+++ Dell-market/main-page/main.js
@@ -1,7 +1,7 @@
 const humb_btn = document.querySelector('.hamburger')
 const burger_active = document.querySelector('header')
 
-humb_btn.addEventListener('click', function(){
+humb_btn.addEventListener('click', () => {
     burger_active.classList.toggle('active')
 })
 window.onclick = function(event) {
@@ -9,3 +9,23 @@ window.onclick = function(event) {
         burger_active.classList.toggle('active')
     }
 }
+
+const inpPhone = document.querySelector('#input-phone')
+const inpName = document.querySelector('#input-name')
+const inpComment = document.querySelector('#input-comment')
+inpPhone.addEventListener('input', () => {
+  const v = inpPhone.value.replace(/[^0-9.]+/g, '')
+  inpPhone.value = v;
+});
+
+const contact_btn = document.querySelector('#contact-us')
+contact_btn.addEventListener('click', () => {
+    var regex=/^[А-Яа-яЁё -]+$/
+    if (inpName.value.length > 3 && inpName.value.match(regex)) {
+        inpName.parentElement.classList.remove("error")
+    } else {
+        inpName.parentElement.classList.add("error")
+    }
+    inpPhone.value.length == 9 ? inpPhone.parentElement.classList.remove("error") : inpPhone.parentElement.classList.add("error")
+    inpComment.value.length >= 10 ? inpComment.parentElement.classList.remove("error") : inpComment.parentElement.classList.add("error")
+})
